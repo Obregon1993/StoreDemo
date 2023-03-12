eb4f833487c410fe2766151fc3e02cf7dce75075
Carlos Obregon <obregonlara1993@gmail.com>
2023-03-11 20:54:10 -0500

    adding checkout button content

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import "../styles/components/Checkout.css";
 
 export default function Checkout() {
-  return <div>Checkout</div>;
+  return (
+    <div className="Checkout">
+      <div className="Checkout-content">
+        <h3>Shop List</h3>
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>Item Name</h4>
+            <span>$10</span>
+          </div>
+          <button type="button">Delete</button>
+        </div>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Total Price: $10</h3>
+        <button type="button">Proceed to checkout </button>
+      </div>
+    </div>
+  );
 }
